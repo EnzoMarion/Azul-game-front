372961007725fb825911d51e7cd0122a4ca020ab
EnzoMarion <marionenzo26@gmail.com>
2026-05-21 16:38:45 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import Lobby from "./pages/Lobby/Lobby";
+import Rules from "./pages/Rules/Rules";
 import Game from "./pages/Game/Game";
-import socket, { setMyPlayerIndex } from "./socket";
+import socket from "./socket";
 
 const AutoRejoin = ({ children }) => {
     const navigate = useNavigate();
@@ -33,17 +35,19 @@ const AutoRejoin = ({ children }) => {
     return children;
 };
 
-const App = () => {
+function App() {
     return (
         <BrowserRouter>
             <AutoRejoin>
                 <Routes>
-                    <Route path="/" element={<Lobby />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/lobby" element={<Lobby />} />
+                    <Route path="/rules" element={<Rules />} />
                     <Route path="/game" element={<Game />} />
                 </Routes>
             </AutoRejoin>
         </BrowserRouter>
     );
-};
+}
 
 export default App;
